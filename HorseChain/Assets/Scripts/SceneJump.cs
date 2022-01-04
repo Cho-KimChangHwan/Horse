ed3ee4faaa74e4f13fa836353ba9f658a39d08e0
Seochangh2 <nomm89@naver.com>
2022-01-04 23:47:02 +0900

    씬 추가

---
+++ HorseChain/Assets/Scripts/SceneJump.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void LoadFarmScene()
+   {
+       SceneManager.LoadScene(0);
+   }
+   public void LoadShopScene()
+   {
+       SceneManager.LoadScene(1);
+   }
+   public void LoadRacingScene()
+   {
+       SceneManager.LoadScene(2);
+   }
+   public void LoadContestScene()
+   {
+       SceneManager.LoadScene(3);
+   }
+   public void LoadStartScene()
+   {
+       SceneManager.LoadScene(4);
+   }
+}
