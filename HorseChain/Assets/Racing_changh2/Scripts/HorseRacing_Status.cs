7cf1563f95a7db80ed1c766bc7e0b4a0eedced76
Seochangh2 <nomm89@naver.com>
2022-01-10 00:05:21 +0900

    서창희 말 진화시키는중
    4단계 나눈거 자유롭게 이어지기해야함 (딕셔너리 사용)

--- HorseChain/Assets/Racing_changh2/Scripts/HorseRacing_Status.cs
+++ HorseChain/Assets/Racing_changh2/Scripts/HorseRacing_Status.cs
@@ -10,8 +10,8 @@ public class HorseRacing_Status : MonoBehaviour
     // 말의 능력치를 저장
     public struct Status
     {
-        public int speed , accel , hp , agility , consistency;
-        public Status(int s,int a,int h,int ag,int c)
+        public float speed , accel , hp , agility , consistency;
+        public Status(float s,float a,float h,float ag,float c)
         {
             this.speed = s;
             this.accel = a;
@@ -20,41 +20,100 @@ public class HorseRacing_Status : MonoBehaviour
             this.consistency = c;
         }
     }
-    bool ishalf; // 레일의 절반을 뛰었는지 판단
+    Dictionary<string,bool> horseLocation = new Dictionary<string, bool>();
+    Status horseStatus;
+
+    bool isHalf; // 레일의 절반을 뛰었는지 판단
+    float rotateTime;
+    // 회전에 필요한 변수 및  오브젝트
+    float radius;
+    bool isRotate;
+    public GameObject round1,round2;
+    Vector3 startPosition;
     void Start()
     {
-        ishalf = false;
+        AddLocation();
+        horseStatus = new Status(2.0f,1.0f,1.0f,1.0f,1.0f);
+        isHalf =false;
+        isRotate =false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
          
-        run();
+        Run();
     }
 
-    void run()
+    void Run()
     {
         Vector3 currentPosition = transform.position;
-        if(currentPosition.x > -22 && currentPosition.x < 18 && !ishalf)
-        {
-            transform.position = Vector3.MoveTowards(transform.position , 
-                                        new Vector3(18,currentPosition.y,currentPosition.z),0.15f);
-        }
-        else if(currentPosition.x > -22 && currentPosition.x < 18 && ishalf)
-        {
-            transform.position = Vector3.MoveTowards(transform.position , 
-                                        new Vector3(-22,currentPosition.y,currentPosition.z),0.1f); 
-        }
-        /*
-        else if()
-        {
 
-        }
-        else if()
+        if(currentPosition.x >= 38  && !isRotate)
         {
-            
-        */
-        
+            isRotate = true;
+            isHalf = true;
+            radius = Vector3.Distance( currentPosition , round1.transform.position);
+            startPosition = currentPosition;
+            Debug.Log("헤이");
+        }
+        else if(currentPosition.x <= -42 && isHalf && !isRotate)
+        {
+            isRotate = true;
+            radius = Vector3.Distance( currentPosition , round2.transform.position);
+            startPosition = currentPosition;
+            Debug.Log("헤2이");
+        }
+        else if(isRotate)
+        {
+            Rotate();
+        }
+        else if(currentPosition.x > -22 && currentPosition.x <= 38 && !isHalf && !isRotate)
+        {
+            transform.position = Vector3.MoveTowards(currentPosition , 
+                                        new Vector3(38,currentPosition.y,currentPosition.z),10f*horseStatus.speed* Time.deltaTime);
+            rotateTime=0;
+            isRotate=false;
+            Debug.Log("헤3이");
+        }
+        else if(currentPosition.x > -22 && currentPosition.x <= 38 && isHalf && isRotate)
+        {
+            transform.position = Vector3.MoveTowards(currentPosition , 
+                                        new Vector3(-22,currentPosition.y,currentPosition.z),10f*horseStatus.speed* Time.deltaTime); 
+            rotateTime=0;
+            isRotate=false;
+            Debug.Log("헤4이");
+        }
+    }
+    void DecisionMake()
+    {
+
+    }
+    void Rotate()
+    {
+        Vector3 currentPosition = transform.position;
+
+        if(isHalf)
+        {
+            rotateTime += horseStatus.speed* Time.deltaTime * 0.5f ;
+            float z = radius * Mathf.Cos(rotateTime);
+            float x = radius * Mathf.Sin(-rotateTime);
+            transform.position = new Vector3(round1.transform.position.x -x,startPosition.y, (round1.transform.position.z -z));
+        }
+        else if(!isHalf)
+        {
+            rotateTime += horseStatus.speed* Time.deltaTime * 0.5f ;
+            float z = radius * Mathf.Cos(-rotateTime);
+            float x = radius * Mathf.Sin(rotateTime);
+            transform.position = new Vector3(round2.transform.position.x -x,startPosition.y, (round2.transform.position.z +z));
+        }
+
+    }
+
+    void AddLocation(){
+        horseLocation.Add("First",true);
+        horseLocation.Add("Second",false);
+        horseLocation.Add("Third",false);
+        horseLocation.Add("Fourth",false);
     }
 }
